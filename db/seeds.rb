0f1a51adea5f08e5de18d9ff95b14f18a8858fae
Karan Pahawa <kpahawa@gmail.com>
2015-01-18 00:03:43 -0500

    updated fix of everything- it all works

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,20 +13,46 @@ c = User.find_by(email: 'c@c.com')
 d = User.find_by(email: 'd@d.com')
 e = User.find_by(email: 'e@e.com')
 f = User.find_by(email: 'f@f.com')
-kpahawa = User.find_or_initialize_by(email: 'kpahawa@gmail.com')
-sql = "UPDATE Users SET latitude=37.79870987,longitude=-122.49419633 WHERE email='bob@bob.com';"
+sql = "UPDATE Users SET latitude=37.746570,longitude=-122.416657 WHERE email='bob@bob.com';"
 ActiveRecord::Base.connection.execute(sql)
-sql = "UPDATE Users SET latitude=37.82954724,longitude=-122.43192352 WHERE email='a@a.com';"
+sql = "UPDATE Users SET latitude=37.754811,longitude=-122.413212 WHERE email='a@a.com';"
 ActiveRecord::Base.connection.execute(sql)
-sql = "UPDATE Users SET latitude=37.8209866,longitude=-122.4598095 WHERE email='b@b.com';"
+sql = "UPDATE Users SET latitude=37.764667,longitude=-122.425175 WHERE email='b@b.com';"
 ActiveRecord::Base.connection.execute(sql)
-sql = "UPDATE Users SET latitude=37.8119052,longitude=-122.43173582 WHERE email='c@c.com';"
+sql = "UPDATE Users SET latitude=37.758119,longitude=-122.453338 WHERE email='c@c.com';"
 ActiveRecord::Base.connection.execute(sql)
-sql = "UPDATE Users SET latitude=37.75647334,longitude=-122.36369386 WHERE email='d@d.com';"
+sql = "UPDATE Users SET latitude=37.717962,longitude=-122.442148 WHERE email='d@d.com';"
 ActiveRecord::Base.connection.execute(sql)
-sql = "UPDATE Users SET latitude=37.81178359,longitude=-122.4396016 WHERE email='e@e.com';"
+sql = "UPDATE Users SET latitude=37.669092,longitude=-122.475350 WHERE email='e@e.com';"
 ActiveRecord::Base.connection.execute(sql)
-sql = "UPDATE Users SET latitude=37.73665126,longitude=-122.40772316 WHERE email='f@f.com';"
+sql = "UPDATE Users SET latitude=37.606561,longitude=-122.389519 WHERE email='f@f.com';"
 ActiveRecord::Base.connection.execute(sql)
-sql = "UPDATE Users SET latitude=37.78532126,longitude=-122.40340347 WHERE email='g@g.com';"
+sql = "UPDATE Users SET latitude=37.572495,longitude=-122.340770 WHERE email='h@h.com';"
 ActiveRecord::Base.connection.execute(sql)
+sql = "UPDATE Users SET latitude=37.692128,longitude=-122.482219 WHERE email='i@i.com';"
+ActiveRecord::Base.connection.execute(sql)
+sql = "UPDATE Users SET latitude=37.756894,longitude=-122.495609 WHERE email='j@j.com';"
+ActiveRecord::Base.connection.execute(sql)
+sql = "UPDATE Users SET latitude=37.783490,longitude=-122.478786 WHERE email='k@k.com';"
+ActiveRecord::Base.connection.execute(sql)
+sql = "UPDATE Users SET latitude=37.801667,longitude=-122.440162 WHERE email='l@l.com';"
+ActiveRecord::Base.connection.execute(sql)
+sql = "UPDATE Users SET latitude=37.807499,longitude=-122.411666 WHERE email='m@m.com';"
+ActiveRecord::Base.connection.execute(sql)
+sql = "UPDATE Users SET latitude=37.795427,longitude=-122.407546 WHERE email='n@n.com';"
+ActiveRecord::Base.connection.execute(sql)
+sql = "UPDATE Users SET latitude=37.422588,longitude=-122.084155 WHERE email='g@g.com';"
+ActiveRecord::Base.connection.execute(sql)
+sql = "UPDATE Users SET latitude=37.787559,longitude=-122.391238 WHERE email='o@o.com';"
+ActiveRecord::Base.connection.execute(sql)
+
+# usera_all = User.all
+#
+# usera_all.each do |user|
+#   puts(user.email)
+#   puts(user.username)
+#
+#   puts(user.longitude)
+#   puts(user.latitude)
+
+# end
